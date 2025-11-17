c7abb5337ae097d8bb3d16138b36897372aae595
naveen.wali <naveenwali34@gmail.com>
2025-11-17 22:42:31 +0530

    exploring get and post

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
+"use strict";
+
 const express = require("express");
 
 const app = express();
+
+app.get("/user", (req, res) => {
+  res.send({ name: "naveen", age: 65 });
+});
+
+app.post("/user", (req, res) => {
+  res.send("saved in the database");
+});
 
 app.use("/test", (req, res) => {
   res.send("wlecome to test page ");
 });
 
-app.use("/", (req, res) => {
-  res.send("welcome to the home page ");
-});
-
-app.listen(7777, () => {
+app.listen(3000, () => {
   console.log("server is started");
 });

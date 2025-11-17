19ce31236d7b2d06a82162fe53c7051cc02cc8d8
naveen.wali <naveenwali34@gmail.com>
2025-11-17 23:09:32 +0530

    exploring query params and params

--- src/app.js
+++ src/app.js
@@ -12,6 +12,16 @@ app.post("/user", (req, res) => {
   res.send("saved in the database");
 });
 
+app.get("/product", (req, res) => {
+  console.log("/product", req.query);
+  res.send(req.query);
+});
+
+app.get("/device/:userId", (req, res) => {
+  console.log("/device", req.params);
+  res.send(req.params);
+});
+
 app.use("/test", (req, res) => {
   res.send("wlecome to test page ");
 });

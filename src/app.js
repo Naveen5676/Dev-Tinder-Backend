65b1ac7661be4ed22c7d71780c6f82cc6932d9f9
naveen.wali <naveenwali34@gmail.com>
2025-11-23 14:10:37 +0530

    Exploring error handling using try catch & app.use

--- src/app.js
+++ src/app.js
@@ -4,21 +4,25 @@ const express = require("express");
 
 const app = express();
 
-const { adminAuth, userAuth } = require("./middlewares/auth");
 
-app.use("/admin", adminAuth);
+// first preference is to use try catch to handle the error 
+app.get('/user', (req, res)=>{
+  try{
+     // logic to get user list from db
+      res.send('user list sent ')    
+  }catch(error){
+        res.status(500).send('some thing went wrong ')
+  }
+})
 
-app.get("/admin/userList", (req, res) => {
-  res.send("all user data sent");
-});
 
-app.get("/user/list", userAuth, (req, res) => {
-  res.send("user list sent");
-});
-
-app.get("/login", (req, res) => {
-  res.send("login in user");
-});
+// so this is wild card error is try catch is not defiend then we use this at the end of the code in order to catch the error 
+// the error should be at the first in order use err in the parameters 
+app.use('/', (err , req , res, next)=>{
+  if(err){
+    res.status(500).send("something went wrong ")
+  }
+})
 
 app.listen(3000, () => {
   console.log("server is started");

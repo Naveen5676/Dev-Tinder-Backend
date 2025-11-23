5539af289b56310423e1ed41f033f8e6e354830e
naveen.wali <naveenwali34@gmail.com>
2025-11-23 23:38:26 +0530

    storing the data in db

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ const app = express();
 
 const mongodb = require("./config/database");
 
+const User = require("./models/user")
+
+app.post('/signup', async(req, res)=>{
+  // create a new instace of the user modal 
+  const user = new User({
+    firstName: "naveen",
+    lastName: "wali",
+    emailId: "naveen@gmail.com",
+    password: "naveen@123"
+  })
+
+  try{
+         
+    await user.save()
+    res.send("user signup successfully")
+  }catch(err){
+    res.status(400).send("some thing went wrong ")
+  }
+})
+
 mongodb()
   .then(() => {
     console.log("database connected successfully ");

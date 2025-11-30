2e75534ecf3f15150356929e60dcbafd246ccac6
naveen.wali <naveenwali34@gmail.com>
2025-11-30 19:13:37 +0530

    Exploring validation in the schema the built in and custom one's

--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,55 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 50,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (![male, female].includes(value)) {
+          throw new error("geneder data is not valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      defaut:
+        "https://cdn.vectorstock.com/i/1000v/66/13/default-avatar-profile-icon-social-media-user-vector-49816613.jpg",
+    },
+    about: {
+      type: String,
+      default: "this is default data in about section",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
